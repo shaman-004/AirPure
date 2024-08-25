1b3e02f0b5209fef9139100e38710cdf7c494311
shaman <shamanbusiness2004@gmail.com>
2024-08-25 16:02:43 +0530

    changes in config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  // Get the repository name from the GitHub environment variables
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 module.exports = {
- 
-    trailingSlash: true,
-  }
-  
+  trailingSlash: true, // Ensures URLs end with a trailing slash
+  assetPrefix: assetPrefix, // Prefixes asset URLs with the repository name
+  basePath: basePath, // Sets the base path to the repository name
+  images: {
+    unoptimized: true, // Disable Next.js image optimization for static site exports
+  },
+};
